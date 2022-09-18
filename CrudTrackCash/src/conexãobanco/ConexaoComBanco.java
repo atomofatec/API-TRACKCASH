82be9904d19028db20813af3d6fff94ebb8379b8
Átomo <100942191+atomofatec@users.noreply.github.com>
2022-09-18 18:24:18 -0300

    Produto do API

---
+++ CrudTrackCash/src/conexãobanco/ConexaoComBanco.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package conexãobanco;
+import java.sql.Connection;
+// Providencia a Conexao e Processamento de Dados no Java
+import java.sql.DriverManager;
+/*É uma classe estatica no Java que define do Java Database
+Connectivity (JDBC) drivers que estão disponiveis para o uso na aplicação */
+import java.sql.SQLException;
+/*É utilizado para providenciar informação sobre erros no acesso a database do banco
+ou outros erros.*/
+
+public class ConexaoComBanco {
+    public Connection getConnection(){
+        try {
+            return DriverManager.getConnection("jdbc:mysql://localhost/bancoApiii?serverTimezone=UTC","root","fatec");          
+        }
+        catch(SQLException excecao){
+            throw new RuntimeException(excecao);
+        }
+    }
+}
